5f212726452135bebf29dee1f3e392f7eb5aa070
David Rauch <mail@davidrauch.at>
2018-01-27 13:21:30 +0100

    Add copy benchmark

---
+++ benchmarks/01_cp/copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Sizes: 1kb, 10kb, 1mb, 10mb
+array=( 1 10 1024 10240 )
+
+for i in "${array[@]}"
+do
+	# Create file
+  	dd if=/dev/urandom of=/tmp/testmount/test.dat bs=1k count=$i
+
+	# Create report with iterations
+	(perf stat -B -r 1000 cp /tmp/testmount/test.dat /tmp/testmount/test_copy.dat > /dev/null) 2> results/btrminix_copy_$i.out
+done
+
+# Alternatively: Create perf.data
+# sudo perf record -o cp/minix-copy-1000_perf.data -c 1000 cp --reflink /tmp/testmount/test.dat /tmp/testmount/test_copy.dat
+# and then perf report
