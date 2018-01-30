ed932c5291f06a508b0483ddff97df8495ae8d4f
Christian Werling <crwerling@gmail.com>
2018-01-30 23:19:30 +0100

    Results for write benchmarks based on tmpfs

--- benchmarks/02_dd/command.sh
+++ benchmarks/02_dd/command.sh
@@ -2,4 +2,12 @@
 
 rm /tmp/testmount/testfile
 touch /tmp/testmount/testfile
-dd if=/dev/zero of=/tmp/testmount/testfile bs=1M count=256 oflag=direct 2> results/minix-256M.out
+
+# dd if=/dev/urandom of=/tmp/testmount/testfile bs=1M count=256 # btrminix-cloned only
+
+for i in `seq 10`
+do
+	# cp --reflink /tmp/testmount/testfile /tmp/testmount/testfile2 # btrminix-cloned only
+	dd if=/dev/zero of=/tmp/testmount/testfile bs=1M count=256 2>> results/btrminix-tmpfs-zero-256M-10.out
+	# rm /tmp/testmount/testfile2 # btrminix-cloned only
+done
