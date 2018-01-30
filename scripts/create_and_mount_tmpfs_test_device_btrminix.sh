0a415172ec7dfa9d89ec8c771614e27ae76499a3
Christian Werling <crwerling@gmail.com>
2018-01-30 23:18:25 +0100

    Fixed tmpfs-script

--- scripts/create_and_mount_tmpfs_test_device_btrminix.sh
+++ scripts/create_and_mount_tmpfs_test_device_btrminix.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
+
 sudo umount /tmp/testmount
 
-rm -rf /tmp/tmpfs 2>/dev/null
-rm -rf /tmp/testmount 2>/dev/null
+sudo losetup -d /dev/loop0
+
+sudo umount /tmp/tmpfs
+
+sudo rm -rf /tmp/tmpfs
+sudo rm -rf /tmp/testmount
+
+# finished tidying up
 
 mkdir /tmp/tmpfs
 sudo mount -t tmpfs none /tmp/tmpfs
 
-dd if=/dev/zero of=/tmp/tmpfs/testdevice bs=1M count=256
+dd if=/dev/zero of=/tmp/tmpfs/testdevice bs=1M seek=699 count=1
 
-mkdir /tmp/testmount
-
-sudo losetup -d /dev/loop0
 sudo losetup /dev/loop0 /tmp/tmpfs/testdevice
 
 sudo ../util/mkfs.minix -s 3 /dev/loop0
+#sudo mkfs.minix -3 /dev/loop0
+#sudo mkfs.btrfs /dev/loop0
 
-sudo mount -t btrminix /dev/loop0 /tmp/testmount
+mkdir /tmp/testmount
+sudo mount -t btrminix -o loop /tmp/tmpfs/testdevice /tmp/testmount
 sudo chown 1000:1000 /tmp/testmount
